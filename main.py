9262f02f05e49e07d00c027289b65fd4690ef5a0
Alexey <lehacomp16@gmail.com>
2026-06-24 14:50:05 +0300

    v1.0.6

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_weather():
     print(f"Дата и время: {now.strftime('%d.%m.%Y %H:%M:%S')}")
     print("-" * 70)
     current_temp = data["current"]["temperature_2m"]
-    current_uv = data["current"]["uv_index"]
+    current_uv = round(data["current"]["uv_index"])
     current_pha = round((data["current"]["pressure_msl"]) * 0.750062)
     current_rain = data["current"]["precipitation_probability"]
     print(f"Город: {city}")
@@ -26,18 +26,21 @@ def get_weather():
     print(f"Текущее давление: {current_pha}мм")
     print(f"Вероятность осадков: {current_rain}%")
     print("-" * 70)
-    print("Прогноз на оставшуюся часть дня:")
+    print("Прогноз на 24 часа:")
     print(f"{'Время':<8} | {'Температура':<8} | {'Вероятность осадков':<8} | {'УФ-индекс':<10} | {'Давление':<12}")
     print("-" * 70)
     hourly = data["hourly"]
     current_hour_str = now.strftime("%Y-%m-%dT%H:00")
+    hours_printed = 0
     for i, time_str in enumerate(hourly["time"]):
-        if (time_str >= current_hour_str and time_str.split("T")[0] == now.strftime("%Y-%m-%d")):
+        if time_str >= current_hour_str:
             display_time = time_str.split("T")[1]
             temp = hourly["temperature_2m"][i]
             rain_prob = hourly["precipitation_probability"][i]
             hpa = round((hourly["pressure_msl"][i]) * 0.750062)
-            uv = hourly["uv_index"][i]
+            uv = round(hourly["uv_index"][i])
             print(f"{display_time:<8} | {f'{temp}°C':<11} | {f'{rain_prob}%':<19} | {f'{uv}':<10} | {f'{hpa} мм'}")
+            hours_printed += 1
+            if hours_printed == 24: break
 if __name__ == "__main__":
     get_weather()
