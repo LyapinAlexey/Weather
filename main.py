49d6bdc5319779b0697d27ab1d476d419744d154
Alexey <lehacomp16@gmail.com>
2026-06-24 16:28:12 +0300

    v1.2.0

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import requests
 import sys
 import platform
 import subprocess
-def get_weather():
+for_printing = False
+def get_weather(for_printing):
     city = "Москва"
     lat = 55.675093
     lon = 37.550441
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,pressure_msl,precipitation_probability,uv_index&hourly=temperature_2m,precipitation_probability,uv_index,pressure_msl&timezone=auto"
+    url = (f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}"
+    f"&current=temperature_2m,pressure_msl,precipitation_probability,uv_index"
+    f"&hourly=temperature_2m,precipitation_probability,uv_index,pressure_msl"
+    f"&daily=temperature_2m_mean,precipitation_sum,uv_index_max,wind_speed_10m_max,wind_gusts_10m_max"
+    f"&timezone=auto")
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -18,7 +23,8 @@ def get_weather():
         return
     now = datetime.now()
     print(f"Дата и время: {now.strftime('%d.%m.%Y %H:%M:%S')}")
-    print("-" * 70)
+    if for_printing: print("-" * 92)
+    else: print("-" * 70)
     current_temp = data["current"]["temperature_2m"]
     current_uv = round(data["current"]["uv_index"])
     current_pha = round((data["current"]["pressure_msl"]) * 0.750062)
@@ -28,10 +34,12 @@ def get_weather():
     print(f"Текущий УФ-индекс: {current_uv}")
     print(f"Текущее давление: {current_pha}мм")
     print(f"Вероятность осадков: {current_rain}%")
-    print("-" * 70)
+    if for_printing: print("-" * 92)
+    else: print("-" * 70)
     print("Прогноз на 24 часа:")
     print(f"{'Время':<8} | {'Температура':<8} | {'Вероятность осадков':<8} | {'УФ-индекс':<10} | {'Давление':<12}")
-    print("-" * 70)
+    if for_printing: print("-" * 92)
+    else: print("-" * 70)
     hourly = data["hourly"]
     current_hour_str = now.strftime("%Y-%m-%dT%H:00")
     hours_printed = 0
@@ -45,19 +53,36 @@ def get_weather():
             print(f"{display_time:<8} | {f'{temp}°C':<11} | {f'{rain_prob}%':<19} | {f'{uv}':<10} | {f'{hpa} мм'}")
             hours_printed += 1
             if hours_printed == 24: break
+    if for_printing:
+        print("-" * 92)
+        print("Прогноз на неделю:")
+        print(f"{'Дата':<10} | {'Сред. темп.':<12} | {'Объем осадков':<15} | {'Макс. УФ':<8} | {'Скорость ветра':<15} | {'Порывы ветра':<14}")
+        print("-" * 92)
+        daily = data["daily"]
+        for i, date_str in enumerate(daily["time"]):
+            parsed_date = datetime.strptime(date_str, "%Y-%m-%d").strftime("%d.%m.%Y")
+            t_mean = daily["temperature_2m_mean"][i]
+            rain_sum = daily["precipitation_sum"][i]
+            uv_max = round(daily["uv_index_max"][i])
+            wind_max = daily["wind_speed_10m_max"][i]
+            gusts_max = daily["wind_gusts_10m_max"][i]
+            print(f"{parsed_date:<10} | {f'{t_mean}°C':<12} | {f'{rain_sum} мм':<15} | {f'{uv_max}':<8} | {f'{wind_max} км/ч':<15} | {f'{gusts_max} км/ч':<14}")
+    print("-" * 92)
 if __name__ == "__main__":
     print("Нужно ли напечатать прогноз?(Используется стандартный принтер)")
     way = input("0 - нет; 1 - да: ")
     print("-" * 70)
     if way == "1":
+        for_printing = True
         filename = "weather_report.txt"
         original_stdout = sys.stdout
         current_os = platform.system()
-        file_encoding = "utf-16" if current_os == "Windows" else "utf-8"
-        with open(filename, "w", encoding=file_encoding) as f:
-            sys.stdout = f
-            get_weather()
-        sys.stdout = original_stdout
+        file_encoding = "cp1251" if current_os == "Windows" else "utf-8"
+        try:
+            with open(filename, "w", encoding=file_encoding) as f:
+                sys.stdout = f
+                get_weather(for_printing)
+        finally: sys.stdout = original_stdout
         try:
             if current_os == "Windows":
                 subprocess.run(f'notepad.exe /p "{filename}"', shell=True, check=True)
@@ -67,4 +92,6 @@ if __name__ == "__main__":
                 print("Документ успешно отправлен в очередь печати UNIX!")
             else: print(f"Ошибка: Операционная система {current_os} не поддерживается для печати.")
         except Exception as e: print(f"Не удалось отправить на печать: {e}")
-    else: get_weather()
+    else:
+        for_printing = False
+        get_weather(for_printing)
